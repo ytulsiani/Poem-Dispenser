f43cf2ace64ca736be71c3eafe4c77b0fc8eeab0
Yash <ytulsiani3@gatech.edu>
2017-10-17 19:52:53 -0400

    fml

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const BootBot = require('bootbot');
 
 const bot = new BootBot({
-  accessToken: process.env.FB_PAGE_ACCESS_TOKEN
+  accessToken: process.env.FB_PAGE_ACCESS_TOKEN,
   verifyToken: process.env.FB_VERIFY_TOKEN,
   appSecret: process.env.FB_APP_SECRET
   webhook: '/webhook/'
