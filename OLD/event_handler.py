cb4755788f674ba5c39656b2f0d320d6f66cfa48
Yash <ytulsiani3@gatech.edu>
2017-10-17 19:26:49 -0400

    new

---
+++ OLD/event_handler.py
@@ -0,0 +1,9 @@
+import sys
+
+def main():
+	text = sys.argv[1]
+	
+	print('pied piper')
+
+if __name__ == '__main__':
+    main()
--- event_handler.py
+++
@@ -1,8 +0,0 @@
-import sys
-
-def main():
-	text = sys.argv[1]
-	print('pied piper') 
-
-if __name__ == '__main__':
-    main()
